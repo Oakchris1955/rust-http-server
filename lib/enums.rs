b19ab1f8dc73019c8de9e310fc9774132c9a584a
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-05-21 18:41:26 +0300

    Implement basic HttpStatus enum

--- lib/enums.rs
+++ lib/enums.rs
@@ -1,5 +1,31 @@
 use std::fmt;
 
+pub enum HttpStatus {
+	OK,
+	BadRequest
+}
+
+impl HttpStatus {
+	/// Initialize a `HttpStatus` by passing a `usize` to it (for example, pass `200` to get a `HttpStatus::OK` variant)
+	pub fn new(status: usize) -> Option<Self> {
+		match status {
+			200 => Some(Self::OK),
+			400 => Some(Self::BadRequest),
+			_ => None
+		}
+	}
+}
+
+impl fmt::Display for HttpStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", match self {
+			Self::OK => 200,
+			Self::BadRequest => 400
+		})
+    }
+}
+
+
 /// A rather simple enum implementing an initialization method and the `Display` trait
 pub enum HttpMethod {
 	GET,
