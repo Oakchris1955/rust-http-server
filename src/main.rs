ac30964aeca490682faf5e14a64e8d85d663c385
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-05-26 20:14:15 +0300

    Update main.rs
    Example now uses the new HttpServer architecture

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
 use oak_http_server::HttpServer;
 
 fn main() {
-	let server = HttpServer::new("localhost", 2300 as u16);
+	let mut server = HttpServer::new("localhost", 2300 as u16);
+	server.on("/test", |request, response| {
+		response.send(format!(
+			"Your current query options are:\n{}",
+			request.target.queries.iter().map(|x| {
+				format!("{}: {}\n", x.name, x.value.clone().unwrap_or(String::new()))
+			}).collect::<String>()
+		))
+	});
+
 	server.start();
 }
